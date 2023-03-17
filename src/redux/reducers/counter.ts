f82086eedb12aad031021c42709acb77d102b23c
YazanAlhalabi <yazanalhalabi1@gmail.com>
2023-03-17 16:07:54 +0200

    added MUI

--- src/redux/reducers/counter.ts
+++ src/redux/reducers/counter.ts
@@ -6,11 +6,9 @@ export default function counterReducer(state = initialState, action: any) {
     case INCREMENT: {
       return state + 1
     }
-
     case DECREMENT: {
       return state - 1
     }
-
     default:
       return state
   }
